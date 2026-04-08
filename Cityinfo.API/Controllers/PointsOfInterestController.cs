ed5335b9457cac8ac6f2933952aac28ba0f7a6e4
Your name <You@example.com>
2026-04-08 07:37:40 +0530

    Creating a resource and validating inputs

--- Cityinfo.API/Controllers/PointsOfInterestController.cs
+++ Cityinfo.API/Controllers/PointsOfInterestController.cs
@@ -18,7 +18,7 @@ namespace Cityinfo.API.Controllers
             }
             return Ok(city.PointsOfInterest);
         }
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointofinterestid)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -32,6 +32,39 @@ namespace Cityinfo.API.Controllers
                 return NotFound();
             }
             return Ok(pointofinterest);
+        }
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, pointOfInterestCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var maxpointofinterestid = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+            var finalpointofinterest = new PointOfInterestDto()
+            {
+                Id = ++maxpointofinterestid,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            city.PointsOfInterest.Add(finalpointofinterest);
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterest = finalpointofinterest.Id
+                },
+                finalpointofinterest);
+        }
+        [HttpPut("pointofinterestid")]
+        public ActionResult UpdatePointOfInterest(int cityId, int pointofInterestId, pointOfInterestForUpdateCtocs pointOfInterest)
+        {
+
         }
     }
 }
---
+++ Cityinfo.API/Model/pointOfInterestCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cityinfo.API.Model
+{
+    public class pointOfInterestCreationDto
+    {
+        [Required(ErrorMessage ="You should provide a Name")]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+    }
+}
