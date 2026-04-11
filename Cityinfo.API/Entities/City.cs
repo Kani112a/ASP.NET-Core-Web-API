2e458e004d1d80ca6d8b759eebdec56ab33f2adb
Your name <You@example.com>
2026-04-11 11:35:55 +0530

    Created entity classes

---
+++ Cityinfo.API/Entities/City.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cityinfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public ICollection<pointOfInterest> pointOfInterests { get; set; } = new List<pointOfInterest>();
+        public City(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ Cityinfo.API/Entities/pointOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cityinfo.API.Entities
+{
+    public class pointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+        public pointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
